e7dc0162c620e2cc5f0b5ffec681ff06a9e99998
Michael Truog <mjtruog@gmail.com>
2015-02-03 16:08:29 -0800

    first commit

---
+++ README.markdown
@@ -0,0 +1,6 @@
+[![Build Status](https://secure.travis-ci.org/CloudI/cloudi_service_validate.png?branch=master)](http://travis-ci.org/CloudI/cloudi_service_validate)
+
+If you are looking to see if you need this repository, please refer to the [`cloudi_core` repository](https://github.com/CloudI/cloudi_core#about).
+
+If you are looking for CloudI service integration documentation, [go here](https://github.com/CloudI/CloudI#integration).
+
---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule CloudIServiceValidate do
+  use Mix.Project
+
+  def project do
+    [app: :cloudi_service_validate,
+     version: "1.4.0",
+     language: :erlang,
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    [{:cloudi_core, "~> 1.4.0"}]
+  end
+
+  defp description do
+    "Erlang/Elixir Cloud Framework Validate Service"
+  end
+
+  defp package do
+    [files: ~w(src doc rebar.config README.markdown),
+     contributors: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"Website" => "http://cloudi.org",
+              "GitHub" => "https://github.com/CloudI/" <>
+                          "cloudi_service_validate"}]
+   end
+end
