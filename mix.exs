8d1f88a64c2f658ce749458c78a2125823e82481
Michael Truog <mjtruog@protonmail.com>
2021-05-27 02:59:26 -0700

    Version 2.0.2.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CloudIServiceValidate do
 
   def project do
     [app: :cloudi_service_validate,
-     version: "2.0.1",
+     version: "2.0.2",
      language: :erlang,
      erlc_options: [
        :debug_info,
@@ -29,7 +29,7 @@ defmodule CloudIServiceValidate do
   end
 
   defp deps do
-    [{:cloudi_core, "~> 2.0.1"}]
+    [{:cloudi_core, "~> 2.0.2"}]
   end
 
   defp description do
